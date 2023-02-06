e370b806cbb74571d0d24cb40318d46b1dac3c05
Josué Almonasi <josue.almonasi@hotmail.com>
2023-02-05 20:06:54 -0600

    fixed relative path

--- context/ui/index.ts
+++ context/ui/index.ts
@@ -1,3 +1,3 @@
-export * from './UIContext'
-export * from './UIContextProvider'
-export * from './UIReducer'
+export * from 'context/ui/UIContext'
+export * from 'context/ui/UIContextProvider'
+export * from 'context/ui/UIReducer'
