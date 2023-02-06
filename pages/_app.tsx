7c56dd6e363a13ea07572d4c5c73da25559669ce
Josué Almonasi <josue.almonasi@hotmail.com>
2023-02-05 21:11:36 -0600

    changed paths

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import { UIContextProvider } from 'context/ui'
 import type { AppProps } from 'next/app'
-import { lightTheme } from 'themes'
 import 'styles/globals.css'
+import { lightTheme } from 'themes'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
