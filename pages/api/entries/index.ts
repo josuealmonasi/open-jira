a4be83f68a0ccc16c64395d11bf2fdee35f467dc
Josué Almonasi <josue.almonasi@hotmail.com>
2023-05-21 16:55:35 -0600

    exported IEntry and added get entries path

---
+++ pages/api/entries/index.ts
@@ -0,0 +1,27 @@
+import { db } from 'database'
+import { Entry, IEntry } from 'models'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data =
+  | {
+      message: string
+    }
+  | IEntry[]
+  | IEntry
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  switch (req.method) {
+    case 'GET':
+      return getEntries(res)
+    default:
+      return res.status(405).end(`Method ${req.method} Not Allowed`)
+  }
+}
+
+const getEntries = async (res: NextApiResponse<Data>) => {
+  await db.connect()
+  const entries = await Entry.find().sort({ createdAt: 'asc' })
+  await db.disconnect()
+
+  res.status(200).json(entries)
+}
