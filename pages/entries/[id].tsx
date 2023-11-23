1523aa8d454c93b4ee09a4baee76179ab2adfe11
Josué Almonasi <josue.almonasi@hotmail.com>
2023-11-22 21:21:46 -0600

    Improvements to the edit entry page

--- pages/entries/[id].tsx
+++ pages/entries/[id].tsx
@@ -1,3 +1,4 @@
+import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined'
 import {
   Button,
   Card,
@@ -11,21 +12,40 @@ import {
   Radio,
   RadioGroup,
   TextField,
+  capitalize,
 } from '@mui/material'
 import { Layout } from 'components/layouts'
-import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined'
-import React from 'react'
 import { EntryStatus } from 'interfaces'
+import { ChangeEvent, useMemo, useState } from 'react'
 
 const validStatus: EntryStatus[] = ['pending', 'completed', 'in-progress']
 
 const EntryPage = () => {
+  const [inputValue, setInputValue] = useState('')
+  const [status, setStatus] = useState<EntryStatus>('pending')
+  const [touched, setTouched] = useState(false)
+  const isNotValid = useMemo(() => !inputValue && touched, [inputValue, touched])
+
+  const onTextFieldChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value)
+  }
+
+  const onChangeStatus = (event: ChangeEvent<HTMLInputElement>) => {
+    setStatus(event.target.value as EntryStatus)
+  }
+
+  const onSave = () => {
+    if (!inputValue) return
+    console.log('Save')
+  }
+
   return (
     <Layout title='test'>
       <Grid container justifyContent={'center'} sx={{ marginTop: 2 }}>
         <Grid item xs={12} sm={8} md={6}>
           <Card>
-            <CardHeader title='test' subheader='creado hace 3 min' />
+            <CardHeader title={`Entry: ${inputValue}`} subheader='creado hace 3 min' />
+
             <CardContent>
               <TextField
                 sx={{ marginTop: 2, marginBottom: 1 }}
@@ -34,23 +54,36 @@ const EntryPage = () => {
                 autoFocus
                 multiline
                 label='test'
-              ></TextField>
+                onChange={onTextFieldChange}
+                value={inputValue}
+                onBlur={() => setTouched(true)}
+                error={isNotValid}
+                helperText={isNotValid && 'Escribe algo'}
+              />
+
               <FormControl>
                 <FormLabel>Status:</FormLabel>
-                <RadioGroup>
+
+                <RadioGroup row onChange={onChangeStatus}>
                   {validStatus.map(status => (
                     <FormControlLabel
                       key={status}
                       value={status}
                       control={<Radio />}
-                      label={status}
+                      label={capitalize(status)}
                     />
                   ))}
                 </RadioGroup>
               </FormControl>
             </CardContent>
             <CardActions>
-              <Button startIcon={<SaveOutlinedIcon />} variant='contained' fullWidth>
+              <Button
+                disabled={!inputValue}
+                startIcon={<SaveOutlinedIcon />}
+                variant='contained'
+                fullWidth
+                onClick={onSave}
+              >
                 Save
               </Button>
             </CardActions>
